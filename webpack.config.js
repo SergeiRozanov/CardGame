dbad011f47652158e0ff2ab9729919b1c283fca6
Sergei <rozanovserg@yandex.ru>
2022-11-23 10:49:04 +0300

    add new changes webpack rows

--- webpack.config.js
+++ webpack.config.js
@@ -2,7 +2,7 @@ const path = require('path');
 const HtmlWebpackPlugin = require('html-webpack-plugin');
 const CopyPlugin = require('copy-webpack-plugin');
 const isProduction = process.env.NODE_ENV === 'production';
-const CssMinimizerPlugin = require('css-minimiser-plugin');
+const CssMinimizerPlugin = require('css-minimizer-webpack-plugin');
 const MiniCssExtractPlugin = require('mini-css-extract-plugin');
 
 module.exports = {
